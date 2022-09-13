a563702afbee24be415d9de35c11abdca9bda888
Tony Gaskell <tony.gaskell@conceptarthouse.com>
2022-09-13 12:55:25 -1000

    Rename output directory

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ join_paths() {
     (IFS=/; echo "$*" | tr -s /)
 }
 
-DIRECTORY_PATH=$(join_paths $ACTIONS_DIRECTORY $GITHUB_OWNER $GITHUB_REPO $RELEASE_TAG)
+DIRECTORY_PATH=$(join_paths $ACTIONS_DIRECTORY $GITHUB_OWNER $GITHUB_REPO@$RELEASE_TAG)
 
 setup_download_directory $DIRECTORY_PATH
 download_action "https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/tarball/$RELEASE_TAG" "$GITHUB_TOKEN" "$DIRECTORY_PATH"
